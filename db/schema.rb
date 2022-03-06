ef1d1c80dadc2e2e090c649b322bfcdebd5603c9
Jakey08 <yuweb1105@gmail.com>
2022-03-07 00:48:54 +0900

    add bcrypt and has-secure-password in user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_142736) do
+ActiveRecord::Schema.define(version: 2022_03_06_152942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_142736) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "pasword_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
