c218233f89621a0cea86950e4762d94176cf79cf
Jakey08 <yuweb1105@gmail.com>
2022-08-13 22:09:49 +0900

    crud todo-app

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks/create', to: 'tasks#create'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
